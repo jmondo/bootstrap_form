1a8fe85b6d3179dba2d86fd9375b1ba168128669
John Gesimondo <jmondo@newrelic.com>
2014-01-17 15:00:36 -0800

    allow adding arbitrary classes to form groups

--- lib/bootstrap_form/form_builder.rb
+++ lib/bootstrap_form/form_builder.rb
@@ -23,10 +23,11 @@ module BootstrapForm
         options = args.extract_options!.symbolize_keys!
 
         label = options.delete(:label)
+        form_group_class = options.delete(:form_group_class)
         label_class = hide_class if options.delete(:hide_label)
         help = options.delete(:help)
 
-        form_group(name, label: { text: label, class: label_class }, help: help) do
+        form_group(name, label: { text: label, class: label_class }, class: form_group_class, help: help) do
           options[:class] = "form-control #{options[:class]}".rstrip
           args << options.except(:prepend, :append)
           input = super(name, *args)
@@ -62,8 +63,9 @@ module BootstrapForm
     end
 
     def form_group(name = nil, options = {}, &block)
-      options[:class] = 'form-group'
-      options[:class] << ' has-error' if has_error?(name)
+      classes = ['form-group', options[:class]]
+      classes << 'has-error' if has_error?(name)
+      options[:class] = classes.compact.join(' ')
 
       html = capture(&block)
       html << generate_help(name, options[:help])
